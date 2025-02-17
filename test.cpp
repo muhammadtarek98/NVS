40197440c9c661d269ad6d15b3518772c98a092e
Your Name <m.aboelmagd@mcsoil.com>
2025-02-17 14:23:49 +0200

    "test"

---
+++ test.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+#include "colmap/base/reconstruction.h"
+#include "colmap/controllers/incremental_mapper.h"
+#include "colmap/feature/extraction.h"
+#include "colmap/util/misc.h"
+int main(){
+    std::cout<<"build done ";
+
+    return 0;
+}
