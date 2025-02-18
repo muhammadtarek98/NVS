98e22f03d693bdd9984ef9128e8844b8a62a2667
muahmmad <muahmmadtarek@git>
2025-02-18 12:49:18 +0200

    test

--- test.cpp
+++ test.cpp
@@ -1,8 +1,8 @@
 #include<bits/stdc++.h>
-#include "colmap/base/reconstruction.h"
-#include "colmap/controllers/incremental_mapper.h"
-#include "colmap/feature/extraction.h"
-#include "colmap/util/misc.h"
+#include <colmap/base/reconstruction.h>
+#include <colmap/controllers/incremental_mapper.h>
+#include <colmap/feature/extraction.h>
+#include <colmap/util/misc.h>
 int main(){
     std::cout<<"build done ";
 
